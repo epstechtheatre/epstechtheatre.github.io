837ce9d33c1f490abb23aa2d0f121170c97669d9
Ben <1123bmac@gmail.com>
2021-08-14 00:52:38 +0000

    update button dict

--- docs/js/svg_button_url_dict.js
+++ docs/js/svg_button_url_dict.js
@@ -304,7 +304,7 @@ var buttonURLDict = [
         names: ["numbers", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     },
     {
-        url: "/Lighting_keyboard_encoders.html",
+        url: "/Lighting_keyboard_encoder_wheels.html",
         names: ["encoders", "encoder1", "encoder2", "encoder3", "encoder4"]
     },
     {
