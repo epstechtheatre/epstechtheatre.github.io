dd0efb578ff75c4d8153e1c32404a9e8c66e51a8
Ben <1123bmac@gmail.com>
2021-08-31 00:21:27 +0000

    new global page arg manager

--- docs/js/customscripts.js
+++ docs/js/customscripts.js
@@ -1,8 +1,30 @@
 const KEYBOARD_SVG_FADE_TIME = 150;
 
+const possibleUriArgs = { //The name of the arg, and whether or not it is enabled
+    pdf: pageNoPDFSetting != "true"
+}
+
 $('#mysidebar').height($(".nav").height());
 
 $( document ).ready(function() {
+    const validReqArgs = {};
+    
+    let reqArgs = window.location.href.split("?")[1];
+    if (reqArgs) {
+        reqArgs = reqArgs.split("&");
+    } else {
+        reqArgs = [];
+    }
+
+    for (arg of reqArgs) {
+        let split = arg.split("=")
+
+        if (split.length === 2) {
+            if (possibleUriArgs[split[0]]) {
+                validReqArgs[split[0]] = split[1];
+            }
+        }
+    }
 
     if (document.getElementsByClassName("display-button-softkey").length > 0
     || document.getElementsByClassName("display-button-keypad").length > 0
@@ -36,61 +58,68 @@ $( document ).ready(function() {
     anchors.add('h2,h3,h4,h5');
 
     // Activate button keypad display overlays
-    $('.display-button-softkey')
-    .mouseenter(function() {
-        showNewKey(this, $(this).data('keyid'), true)
-    })
-    .mouseleave(function() {
-        unShow()
-    })
 
-    $('.display-button-keypad')
-    .mouseenter(function() {
-        showNewKey(this, $(this).data('keyid'), false) //TODO: These currently don't have softkey support (maybe they should)
-    })
-    .mouseleave(function() {
-        unShow()
-    })
-
-    $('.inline-display-button')
-    .mouseenter(function() {
-        showNewKey(this, $(this).data('keyid'), false) //TODO: These currently don't have softkey support (maybe they should)
-    })
-    .mouseleave(function() {
-        unShow()
-    })
-
-
-
-    //PDF View
-    let reqArgs = window.location.href.split("?")[1]
-    if (reqArgs) {
-        reqArgs = reqArgs.split("&")
-    } else {
-        reqArgs = [];
+    if (validReqArgs["pdf"] != "true") {
+        $('.display-button-softkey')
+        .mouseenter(function() {
+            showNewKey(this, $(this).data('keyid'), true)
+        })
+        .mouseleave(function() {
+            unShow()
+        })
+    
+        $('.display-button-keypad')
+        .mouseenter(function() {
+            showNewKey(this, $(this).data('keyid'), false) //TODO: These currently don't have softkey support (maybe they should)
+        })
+        .mouseleave(function() {
+            unShow()
+        })
+    
+        $('.inline-display-button')
+        .mouseenter(function() {
+            showNewKey(this, $(this).data('keyid'), false) //TODO: These currently don't have softkey support (maybe they should)
+        })
+        .mouseleave(function() {
+            unShow()
+        })
     }
 
-    if (reqArgs.some(arg => arg.includes("pdf=true"))) { //Client requested pdf, time to break some stuff
-        if (pageNoPDFSetting != "true") {
-            $("#tg-sb-sidebar").hide();
-            $("#tg-sb-content").toggleClass('col-md-9');
-            $("#tg-sb-content").toggleClass('col-md-12');
-            $("#tg-sb-icon").toggleClass('fa-toggle-on');
-            $("#tg-sb-icon").toggleClass('fa-toggle-off');
+    //PDF View
+    if (validReqArgs["pdf"] == "true") { //Client requested pdf, time to break some stuff
+        $("#tg-sb-sidebar").hide();
+        $("#tg-sb-content").toggleClass('col-md-9');
+        $("#tg-sb-content").toggleClass('col-md-12');
+        $("#tg-sb-icon").toggleClass('fa-toggle-on');
+        $("#tg-sb-icon").toggleClass('fa-toggle-off');
 
-            $("#search-input").hide();
-            $(".githubEditButton").hide();
-            $(".printerViewButton").hide();
-            $(".navbar-right").hide();
+        $("#search-input").hide();
+        $(".githubEditButton").hide();
+        $(".printerViewButton").hide();
+        $(".navbar-right").hide();
+        $("#pageFooterHR").hide()
 
-            $(".container").css({width: "100%"})
-            $(".tags").hide();
-            $("#toc").hide();
-            $(".navbar-static-top").hide()
-            $(".console_command_tip").hide()
-        } else {
-            document.getElementsByClassName("summary")[0].insertAdjacentHTML("beforebegin", "<div class=noPrinterView>Sorry! This page doesn't support printer-friendly viewing.</div")
-            alert("Sorry! This page doesn't support printer-friendly viewing")
+        $(".container").css({width: "100%"})
+        $(".tags").hide();
+        $("#toc").hide();
+        $(".navbar-static-top").hide()
+        $(".console_command_tip").hide()
+
+        $("#page-footer-logo").width("200")
+        $("footer").css({"margin-top": "20px"})
+        $(".footer").css({"text-align": "center"})
+        $(".site-last-updated").hide()
+
+        let d = Date.now();
+        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+        let mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+
+        $(".site-last-updated").after(`<span>Page Accessed: ${`${mo} ${da}, ${ye}`}</span><br><i>${window.location.hostname}${window.location.pathname}</i>`)
+    } else {
+        if (reqArgs.includes("pdf=true")) {
+            document.getElementsByClassName("summary")[0].insertAdjacentHTML("beforebegin", "<div class=noPrinterView>Sorry! This page doesn't have a printer-friendly view. You can still try printing normally and we will try to remove unneeded elements</div")
+            alert("Sorry! This page doesn't have a printer-friendly view")
         }
     }
 });
